23d6cd9a5307213c77c3a5894df272a6250505e9
Joeysan77 <139511090+Joeysan77@users.noreply.github.com>
2025-09-02 16:14:13 +0100

    update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@
 
   document.addEventListener('DOMContentLoaded', function () {
     const search = document.getElementById('search');
-    const container = document.getElementById('cont');
-    const cards = Array.from(document.querySelectorAll('.cont'));
+    const container = document.getElementById('container');
+    const cards = Array.from(document.querySelectorAll('.flexbox'));
 
     if (!search) {
       console.error('Search input #search not found.');
